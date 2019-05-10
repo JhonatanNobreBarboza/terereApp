a5c326f2e29000cde66feb85c4cd0300f1ef6609
jhonatan <jhonatan.souza@novaandradina.org>
2019-05-09 21:41:44 -0400

    Implementação firebase

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const firebase = require('firebase-admin');
 const app = express();
 const router = express.Router();
 //Rotas
 const index = require('./routes/index');
 const terereRoute = require('./routes/terereRouter');
+const serviceAccount = require("./auth/terereif-firebase-adminsdk-42exu-e151edeb8a.json");
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://terereif.firebase.com/"
+  });
+
 app.use('/', index);
 app.use('/terere', terereRoute);
 module.exports = app;
